cd60950725ccf2de1786f91b4ad7dbc83194b48d
Musa <40173603+voyula@users.noreply.github.com>
2020-01-05 08:43:02 +0300

    Update ScopedEloquentUserProviderTest.php

--- tests/Unit/ScopedEloquentUserProviderTest.php
+++ tests/Unit/ScopedEloquentUserProviderTest.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Mockery;
-use Mpyw\ScopedAuth\AuthScopable;
 use Mpyw\ScopedAuth\ScopedEloquentUserProvider;
 use Mpyw\ScopedAuth\Tests\TestCase;
 
@@ -29,7 +28,7 @@ class ScopedEloquentUserProviderTest extends TestCase
     protected $hasher;
 
     /**
-     * @var \Illuminate\Contracts\Auth\Authenticatable|\Illuminate\Database\Eloquent\Model|\Mpyw\ScopedAuth\AuthScopable
+     * @var \Illuminate\Contracts\Auth\Authenticatable|\Illuminate\Database\Eloquent\Model
      */
     protected $model;
 
@@ -43,7 +42,7 @@ class ScopedEloquentUserProviderTest extends TestCase
         /** @var EloquentUserProvider|\Mockery\LegacyMockInterface|\Mockery\MockInterface $parent */
         $parent = Mockery::mock('overload:' . EloquentUserProvider::class);
         $this->hasher = Mockery::mock(Hasher::class);
-        $this->model = Mockery::mock(Model::class . ',' . AuthScopable::class . ',' . UserContract::class);
+        $this->model = Mockery::mock(Model::class . ',' . UserContract::class);
         $this->query = Mockery::mock(Builder::class);
 
         $parent->shouldReceive('newModelQuery')->with(null)->andReturn($this->query);
@@ -59,7 +58,7 @@ class ScopedEloquentUserProviderTest extends TestCase
         /** @var EloquentUserProvider|\Mockery\LegacyMockInterface|\Mockery\MockInterface $parent */
         $parent = Mockery::mock('overload:' . EloquentUserProvider::class);
         $this->hasher = Mockery::mock(Hasher::class);
-        $this->model = Mockery::mock(Model::class . ',' . AuthScopable::class . ',' . UserContract::class);
+        $this->model = Mockery::mock(Model::class . ',' . UserContract::class);
         $this->query = Mockery::mock(Builder::class);
 
         $parent->shouldReceive('newModelQuery')->with($this->model)->andReturn($this->query);
